728bf3385633e3a9848890bb2a4b8526ccc11638
Zhongheng Liu <z.liu@outlook.com.gr>
2024-01-03 22:28:32 +0200

    add other messaging types for finer control

--- src/Chat/types.tsx
+++ src/Chat/types.tsx
@@ -4,8 +4,41 @@ export enum MessageType {
     HELLO,
     DATA,
 }
+export enum SystemMessageCode {
+    REQ,
+    RES,
+    ERR,
+}
+export type HistoryFetchResult = {
+    count: number,
+    items: Array<ChatMessage>,
+}
+export type ErrorResult = {
+    text: string,
+}
+export type TimestampSendRequest = {
+    ts: number,
+}
+export type SystemMessage = {
+    code: SystemMessageCode
+    data: HistoryFetchResult | ErrorResult | TimestampSendRequest
+}
+export type ChatMessage = {
+    fromUserId: string,
+    toUserId: string,
+    content: string,
+    timeMillis: number
+}
+export type HelloMessage = {
+    fromUserId: string,
+    timeMillis: number,
+}
+export type DataMessage = {
+
+}
 export type Message = {
     type: MessageType,
+    // data: SystemMessage | ChatMessage | HelloMessage
     fromUserId: string,
     toUserId: string,
     content: string,
