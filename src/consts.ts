eb76e7203b2f8f6055f895f50ed97145d34d808b
Zhongheng Liu <z.liu@outlook.com.gr>
2024-02-19 22:52:59 +0200

    Preparation for integration with auth/oauth2

--- src/consts.ts
+++ src/consts.ts
@@ -6,6 +6,8 @@ export const endpoints = {
 	subscription: "/sub/chat",
 	history: "/api/v1/msg/",
 	user: "/api/v1/user",
+	auth: "/api/v1/auth",
+	oauth2: "",
 };
 export const contentTypes = {
 	json: {
