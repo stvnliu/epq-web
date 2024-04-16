efcb2204a7314ae19048cea82c1bef5a1940d141
Zhongheng Liu <z.liu@outlook.com.gr>
2024-04-16 10:43:14 +0300

    added cryptographic handler method stub

--- src/crypto.ts
+++ src/crypto.ts
@@ -2,14 +2,18 @@ export const ENCRYPTION_TYPE = "SHA-256";
 
 // Implemented according to https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
 export const digestMessage = async (plaintext: string) => {
-    const textEncoder = new TextEncoder();
-    const digestArray = Array.from(
-        new Uint8Array(
-            await window.crypto.subtle.digest(
-                ENCRYPTION_TYPE, 
-                textEncoder.encode(plaintext)
-                )
-            )
-        )
-    return digestArray.map((byte) => byte.toString(16).padStart(2, "0")).join("");
+	const textEncoder = new TextEncoder();
+	const digestArray = Array.from(
+		new Uint8Array(
+			await window.crypto.subtle.digest(
+				ENCRYPTION_TYPE, 
+				textEncoder.encode(plaintext)
+			)
+		)
+	)
+	return digestArray.map((byte) => byte.toString(16).padStart(2, "0")).join("");
+}
+export const encryptMessage = async (plaintext: string) => {
+	// TODO Method stub
+	return plaintext;
 }
